9fd8ccb68785f689c8ce574caf5774a0fa6c626c
ViVoid <void.Rezcue@gmail.com>
2026-04-07 00:03:40 +0200

    Remove generated files and update .gitignore

--- Objects/gpio.d
+++
@@ -1,3 +0,0 @@
-./objects/gpio.o: GPIO.c GPIO.h TM4C123GH6PM.h \
-  C:\Users\Vitali\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm4.h \
-  system_TM4C123.h
--- Objects/main.d
+++
@@ -1,3 +0,0 @@
-./objects/main.o: main.c TM4C123GH6PM.h \
-  C:\Users\Vitali\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm4.h \
-  system_TM4C123.h SysTick.h GPIO.h ViiROS.h
